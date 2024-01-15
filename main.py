d025fcf7c7d412f10ebb40b08ee190d8cf15e723
royk255 <156660887+royk255@users.noreply.github.com>
2024-01-15 12:17:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,102 @@
-input_file = open(r'dear_prudence.txt', 'r')
-line = input_file.read()
-#input_file.write("----------------------------\n")
-input_file.close()
-new_file = open(r'clear.txt', 'a')
-new_file.write(line)
+line = input("Enter something to calculate: ");
+#line = '5-3*6+8*4'
+op_list = ['+','-','*','/','=','(',')']
+res = []
 
+if "=" not in line:
+    line += "="
+line = line.replace(" ", "")
+
+new_line = ""
+power = False
+if line[0] not in op_list:
+    line = "+"+line
+for q in range(len(line)):
+    if line[q] =="^":
+        for z in range(int(line[q+1])-1):
+            new_line += "*"+line[q-1]
+        power = True
+    else:
+        if power:
+            power = False
+        else:
+            new_line = new_line+line[q]
+line = new_line
+new_line = ""
+
+
+def build_int(s):
+    new_res = ''
+    for i, c in enumerate(s):
+        if (c in op_list):
+            res = float(new_res)
+            return (res, s[i:])
+            break
+        else:
+            new_res += c
+            print(new_res)
+
+
+def calc(line):
+    res = []
+    sum = 0
+    while line:
+        if line[0] == '=':
+            break
+
+        #if line[0] not in op_list and
+        if line[1] == '(':
+            if line[2] not in op_list:
+                temp = calc("+" + line[2:])
+            else:
+                 temp = calc(line[2:])
+            if line[0] == '+':
+                res.append(temp[0])
+            if line[0] == '-':
+                res.append(-1 * temp[0])
+            if line[0] == '*':
+                res.append(res.pop() * temp[0])
+            if line[0] == '/':
+                res.append(res.pop() / temp[0])
+            line = temp[1]
+            while res:
+                sum = sum + res.pop()
+            line = temp[1]
+            continue
+        if line[0] == ')':
+            while res:
+                sum = sum + res.pop()
+            return (sum, line[1:])
+
+
+        print(f'line = {line}')
+        temp = build_int(line[1:])
+
+
+        if line[0] == '+':
+            res.append(temp[0])
+        if line[0] == '-':
+            res.append(-1 * temp[0])
+        if line[0] == '*':
+            res.append(res.pop() * temp[0])
+        if line[0] == '/':
+            res.append(res.pop() / temp[0])
+        line = temp[1]
+    while res:
+        sum = sum + res.pop()
+    return (sum, line[0:])
+
+sum = calc(line)
+print(sum)
+def simle_eq(line):
+    line_n = ""
+    line_x = ""
+
+    while line:
+        if line[0] == '=':
+            break
+        if line[1] not in op_list:
+            build_int(line)
+            line_x += build_int(line)
+        else
 
